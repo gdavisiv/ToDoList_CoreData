4b1121373c6e4e5636cc24bcea6797bd1f256962
George Davis IV <gdavisiv@gmail.com>
2021-01-04 23:24:05 -0500

    Initial Commit

---
+++ ToDoList_CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList_CoreData
+//
+//  Created by George Davis IV on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
